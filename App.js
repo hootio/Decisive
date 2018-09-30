483cedb2a2704d53ce5f29e83a72bf523dfc0ddd
hootanshadmehr <hootan@umich.edu>
2018-09-29 18:02:33 -0700

    Adding delete option functionality

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FlatList, StyleSheet, Text, View, TextInput} from 'react-native';
-import Swipeout from 'react-native-swipeout';
+import SwipeOut from 'react-native-swipeout';
 
 
 export default class Decisive extends React.Component {
@@ -8,15 +8,14 @@ export default class Decisive extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			options : [{key: 'hootan'}, {key: 'dorsa'}, {key: 'maryam'}, {key: 'reza'}],
-			first : true,
+			options : [{key: 'hootan'}, {key: 'dorsa'}, {key: 'maryam'}, {key: 'reza'}]
 		};
 	}
 
 	render() {
 		return (
-			<View style={{flex: 1, marginTop: 22}}>
-				<Text>Hello! Provide some decision options...</Text>
+			<View style={{flex: 1, marginTop: 32}}>
+				<Text style={{fontSize: 27}}>{'Hello there!\t\t\t\t\t\t\u{1F604}\u{2764}\u{1F596}\nCreate some decision options...'}</Text>
 				{/* <View style={styles.container}>
 					<TextInput
 						style={{height: 40}}
@@ -41,11 +40,59 @@ export default class Decisive extends React.Component {
 
 
 class Option extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			activeOptionIndex : null
+		};
+	}
+
 	render() {
+		const swipeSettings = {
+			autoClose : true,
+			onClose : (secId, rowId, direction) => {
+				if (this.state.activeOptionIndex) {
+					this.setState({activeOptionIndex : null});
+				}
+			},
+			onOpen : (secId, rowId, direction) => {
+				this.setState({activeOptionIndex : this.props.item.key});
+			},
+			right : [
+				{
+					onPress : () => {
+						const deletingIndex = this.state.activeOptionIndex;
+						Alert.alert(
+							'Alert!',
+							'Are you sure you want to delete this option?',
+							[
+								{text : 'No', onPress : () => {}, style: 'cancel'},
+								{text : 'Yes', onPress : () => {
+									options.splice(this.props.index, 1);
+									// refresh list
+									this.props.parentOptionsList.refreshOptionsList(deletingIndex);
+								}}
+							],
+							{
+								cancelable : true
+							}
+						);
+					},
+					text : 'Delete',
+					type : 'delete'
+				}
+			],
+			rowId : this.props.index,
+			sectionId : 1
+		};
+
 		return (
-			<View style={{flex: 1, backgroundColor: this.props.index % 2 === 0 ? '#00bfd6' : '#e63e2f'}}>
-				<Text style={styles.option}>{this.props.item.key}</Text>
-			</View>
+			<SwipeOut {...swipeSettings}>
+				<View style={{flex: 1, backgroundColor: this.props.index % 2 === 0 ? '#00bfd6' : '#e63e2f'}}>
+					<Text style={styles.option}>{this.props.item.key}</Text>
+				</View>
+			</SwipeOut>
 		);
 	}
 }
@@ -55,21 +102,26 @@ class OptionsList extends React.Component {
 
 	constructor(props) {
 		super(props);
-		// this.state = {
-		// 	options : [{key: 'hootan'}, {key: 'dorsa'}, {key: 'maryam'}, {key: 'reza'}],
-		// 	first : true,
-		// };
+		this.state = {
+			deletedOptionIndex : null
+		};
 	}
+
+	refreshOptionsList = (deletedIndex) => {
+		this.setState( {
+			deletedOptionIndex : deletedIndex
+		});
+	};
 
 	render() {
 		return (
-			<View style={{flex: 1, marginTop: 22}}>
+			<View style={{flex: 1, marginTop: 16}}>
 				<FlatList
 					data={this.props.options}
 					renderItem={
 						({item, index}) => {
 							return (
-								<Option item={item} index={index}></Option>
+								<Option item={item} index={index} parentOptionsList={this}></Option>
 							);
 						}
 					}
@@ -90,6 +142,6 @@ const styles = StyleSheet.create({
 	option: {
 		color: 'white',
 		padding: 10,
-		fontSize: 16,
+		fontSize: 20,
 	}
 });
